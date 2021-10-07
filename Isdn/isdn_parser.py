e8f387d5d72f330a64071b8f38b2cf919c2e84da
Shahnawaz <31761812+Shahnawaz-lang@users.noreply.github.com>
2021-10-08 00:39:36 +0530

    Update isdn_parser.py
    Added additional stuff: searching

--- Isdn/isdn_parser.py
+++ Isdn/isdn_parser.py
@@ -3,9 +3,13 @@ import argparse
 
 parser = argparse.ArgumentParser(description='This module is used to parse isdn messages')
 parser.add_argument('-f', '--file', help='import a text file', type=str)
+parser.add_argument('-s', '--search', action='store_const', const='True', help='Enable the search flag')
+parser.add_argument('-cgn', '--calling', help='Calling Number', type=str)
+parser.add_argument('-cdn', '--called', help='Called Number', type=str)
 
 args = parser.parse_args()
-isdn.isdn_parse(args.file).ladder()
+
+isdn.isdn_parse(args.file).print_ladder(search=args.search, calllingNum=args.calling, calledNum=args.called)
 
 
 
