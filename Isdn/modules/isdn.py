aef5e9414f004a475903685ada6f2db2ccdd58b4
v_lang <31761812+Shahnawaz-lang@users.noreply.github.com>
2021-10-11 01:44:01 +0530

    Update isdn.py

--- Isdn/modules/isdn.py
+++ Isdn/modules/isdn.py
@@ -1,8 +1,8 @@
 from modules.util.helper import open_file
 from collections import namedtuple
-from modules.template import get_templates
+from modules.template import temp_data 
 
-tmp1, tmp2, tmp3 = get_templates()
+tmp1, tmp2, tmp3 = [*temp_data]
 
 isdn_tinitial = namedtuple('isdn_tinitial', 'callref calling_number called_number')
 isdn_tmgs = namedtuple('isdn_tmgs', 'date sig int prot trans msg pd callref')
@@ -27,15 +27,17 @@ class isdn_ouput:
 class isdn_parse():
 
     def __init__(self, text_file=None):
-
-        self.text_file = list(open_file(text_file))
+        
+        with open(text_file) as file:
+            self.text_file = file.read() 
+            
         self._isdn_linitial = [isdn_tinitial(*t1) for t1 in tmp1.ParseText(self.text_file)]
         self._isdn_lmgs = [isdn_tmgs(*t2) for t2 in tmp2.ParseText(self.text_file)]
         self._isdn_ldial_peers = [isdn_tdial_peers(*t3) for t3 in tmp3.ParseText(self.text_file)]
                
     def parse_calls(self):
         callrefs = [c.callref for c in self._isdn_lmgs if c.msg == "SETUP"]
-        calls, call_nums = {}
+        calls, call_nums = [dict(), dict()]
 
         for mgs in self._isdn_linitial:
             for dp in self._isdn_ldial_peers:
