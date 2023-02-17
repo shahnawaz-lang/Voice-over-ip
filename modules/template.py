b612da2cf4891a56ef91affe34dd89927412791b
v_lang <31761812+Shahnawaz-lang@users.noreply.github.com>
2023-02-18 00:11:28 +0530

    Update template.py

--- modules/template.py
+++ modules/template.py
@@ -7,9 +7,5 @@ with ExitStack() as stack:
   files = [stack.enter_context(open_file(f'modules/templates/{file}')) for file in templates]
   data = list(map(textfsm.TextFSM, files))
 
-with ExitStack() as stack:
-    temps = [stack.enter_context(open_file(f'modules/templates/{file}')) for file in templates]
-    temp_data = list(map(textfsm.TextFSM, temps))
-
  
 
